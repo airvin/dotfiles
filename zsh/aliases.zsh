3b16ec5bbe28bb8fd770963b9564717e33f5c52a
Allison Irvin <allison.irvin@zendesk.com>
2023-04-19 12:36:10 +1000

    Updates

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -2,22 +2,13 @@
 
 # docker
 alias dc='docker-compose'
-alias d='docker'
+alias lzd='lazydocker'
 
 # neovim
 alias vim='nvim'
 
-# git
-alias g='git'
-
 # misc
-alias c='clear'
 alias cat='bat'
-alias ccat='pygmentize -g'
-alias wiki='nvim +VimwikiIndex'
-alias vimf='nvim "+Denite file_rec"'
-alias vimn='nvim +NERDTreeToggle'
-alias find='find . -name'
 alias ls="exa"
 alias la="exa -a"
 alias ll="exa -la"
